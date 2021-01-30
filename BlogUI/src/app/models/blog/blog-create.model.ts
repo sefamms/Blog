1c6ae6c08328d74db1932a88b2fcd16e8756e2af
sefamms <smemis412@gmail.com>
2021-01-30 20:29:21 +0300

    ilk

---
+++ BlogUI/src/app/models/blog/blog-create.model.ts
@@ -0,0 +1,14 @@
+export class BlogCreate {
+
+
+    constructor(
+        public blogId: number,
+        public title: string,
+        public content: string,
+        public photoId?: number
+    ) { }
+
+
+
+
+}
